ea0523275299d691a4c1887138515baa75510182
Angelo Palmieri <palmieri.ang@gmail.com>
2017-07-28 17:47:38 +0100

    input range value

--- src/Saving.jsx
+++ src/Saving.jsx
@@ -6,10 +6,15 @@ class Saving extends Component {
   constructor(props) {
     super(props)
     console.log(props)
+    this.state = {
+      value: this.props.row
+    }
+    this.changeInput = this.changeInput.bind(this)
   }
 
   changeInput(event) {
     console.log(event.target.value)
+    this.setState({value: event.target.value});
   }
 
   render() {
@@ -17,7 +22,7 @@ class Saving extends Component {
     const ListItem = expenditures.map((row, i) =>
       <div className="box-range" key={i} >
         <span>{row.name}</span>
-        <input type="range" min="0" max={row.amount} step="1" onChange={this.changeInput} />
+        <input type="range" min="0" max={row.amount} value={row.amount} step="1" onChange={this.changeInput} />
       </div>
     )
 
